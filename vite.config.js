df5962fc6f426d07de2110548174c962abd86ba8
Chin Phin Shen <iampx95@gmail.com>
2025-07-10 15:55:17 +0800

    fixing deployment configuration

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: "/",
   build: {
     outDir: "dist",
-    sourcemap: false,
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ["react", "react-dom"],
-          bootstrap: ["bootstrap", "react-bootstrap"],
-        },
-      },
-    },
-  },
-  server: {
-    port: 3000,
-    host: true,
   },
 });
